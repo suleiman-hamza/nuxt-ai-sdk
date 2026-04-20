aae674dcc22d78be65c3cd18cb81b9ab076e0c16
suleiman hamza <hamzasuleiman530@gmail.com>
2026-04-20 14:17:40 +0100

    feat/ci: hotfix ci workflow file

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -13,49 +13,11 @@
 </script>
 
 <template>
-  <!-- <div class="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-    <div v-for="m in messages" :key="m.id" class="whitespace-pre-wrap">
-      {{ m.role === 'user' ? 'User: ' : 'AI: ' }}
-      {{ m.content }}
-    </div>
-
-    <div
-      v-if="status === 'submitted' || status === 'streaming'"
-      class="mt-4 text-gray-500"
-    >
-      <div v-if="status === 'submitted'">Loading...</div>
-      <button
-        type="button"
-        class="px-4 py-2 mt-4 text-blue-500 border border-blue-500 rounded-md"
-        @click="stop"
-      >
-        Stop
-      </button>
-    </div>
-
-    <div v-if="error" class="mt-4">
-      <div class="text-red-500">An error occurred.</div>
-      <button
-        type="button"
-        class="px-4 py-2 mt-4 text-blue-500 border border-blue-500 rounded-md"
-        @click="() => reload()"
-      >
-        Retro
-      </button>
-    </div>
-
-    <form @submit="handleSubmit">
-      <input
-        class="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-        v-model="input"
-        placeholder="Say something......"
-        :disabled="disabled"
-      />
-    </form>
-  </div> -->
-  <p class="text-indigo-300">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ut autem nam exercitationem eius
-    nisi cupiditate sequi aut deleniti enim! Ea autem officiis et dolorum. Pariatur deleniti libero
-    omnis dolore.
-  </p>
+  <main>
+    <h1>Welcome to the App</h1>
+    <p>This is a simple Nuxt 4 app.</p>
+    <span>
+      <a href="#">Local Repo Link</a>
+    </span>
+  </main>
 </template>
